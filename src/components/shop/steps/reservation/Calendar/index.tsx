6e8248eebd0da588aadff3e15ba5f5389ac4a541
mrvin100 <vincentyoumssi@gmail.com>
2024-08-17 16:52:17 +0100

    ajout du Calendar avec son propre stock par date

--- src/components/shop/steps/reservation/Calendar/index.tsx
+++ src/components/shop/steps/reservation/Calendar/index.tsx
@@ -3,7 +3,7 @@ import FullCalendar from "@fullcalendar/react";
 import interactionPlugin from "@fullcalendar/interaction";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import { fr } from "date-fns/locale";
-import { startOfToday, addDays, addMonths } from "date-fns";
+import { startOfToday, addDays, addMonths, isSunday, isPast, isSameDay } from "date-fns";
 import { useDateContext } from "@/contexts/dateContext";
 import "../Calendar/style.css";
 import { error } from "@/components/toast";
@@ -11,8 +11,7 @@ import { useWorkplaceContext } from "@/contexts/WorkplaceContext";
 import { usePathname } from "next/navigation";
 
 const Home: React.FC = () => {
-  const { addDate, removeDate, selectedWeekDays, selectedSaturdays } =
-    useDateContext();
+  const { addDate, removeDate } = useDateContext();
   const { workplaces } = useWorkplaceContext();
   const pathname = usePathname();
   const segments = pathname.split("/");
@@ -22,22 +21,21 @@ const Home: React.FC = () => {
     (workplace) => workplace.id === parseInt(lastsegment, 10)
   );
 
-  const [stock, setStock] = useState<number>(
-    selectedWorkplace ? selectedWorkplace.stock : 0
-  );
-  const [selectedDates, setSelectedDates] = useState<string[]>([]);
-  const [individualStock, setIndividualStock] = useState<
-    Record<string, number>
-  >({});
-  const [reservedDates, setReservedDates] = useState<string[]>([]);
   const initialStock = selectedWorkplace ? selectedWorkplace.stock : 0;
+  const [individualStock, setIndividualStock] = useState<Record<string, number>>({});
+  const [selectedDates, setSelectedDates] = useState<string[]>([]);
 
   const calendarRef = useRef<FullCalendar>(null);
   const calendarApiRef = useRef<any>(null);
 
   useEffect(() => {
     if (selectedWorkplace) {
-      setStock(selectedWorkplace.stock);
+      const availableDates = generateAvailableDates();
+      const initialIndividualStock = availableDates.reduce((acc, date) => {
+        acc[date] = initialStock;
+        return acc;
+      }, {} as Record<string, number>);
+      setIndividualStock(initialIndividualStock);
     }
   }, [selectedWorkplace]);
 
@@ -47,21 +45,15 @@ const Home: React.FC = () => {
     if (calendarApi) {
       updateCalendarDates();
     }
-  }, [
-    selectedWeekDays,
-    selectedSaturdays,
-    stock,
-    selectedDates,
-    reservedDates,
-  ]);
+  }, [individualStock]);
 
-  const generateAvailableDates = (stock: number) => {
+  const generateAvailableDates = () => {
     const availableDates: string[] = [];
     const today = startOfToday();
     const threeMonthsLater = addMonths(today, 3);
     let date = today;
 
-    while (date <= threeMonthsLater && availableDates.length < stock) {
+    while (date <= threeMonthsLater) {
       availableDates.push(date.toISOString().split("T")[0]);
       date = addDays(date, 1);
     }
@@ -74,157 +66,71 @@ const Home: React.FC = () => {
     if (calendarApi) {
       calendarApi.removeAllEventSources();
 
-      const today = startOfToday();
-      const availableDates = generateAvailableDates(stock);
+      const events = Object.keys(individualStock).map((dateStr) => {
+        const date = new Date(dateStr);
+        const isPastDate = isPast(date) && !isSameDay(date, new Date());
+        const isInactive = isSunday(date) || isPastDate;
 
-      const events = selectedDates.map((date) => ({
-        id: date,
-        title: individualStock[date]?.toString() || "Disponible",
-        start: date,
-        backgroundColor: "#00E02E",
-        borderColor: "#00E02E",
-        className: "",
-        editable: true,
-        extendedProps: {
-          description: "Disponible",
-        },
-      }));
+        if (isInactive) {
+          return {
+            id: dateStr,
+            start: dateStr,
+            display: 'background',
+            backgroundColor: '#f0f0f0', // Couleur de fond pour les dates inactives
+            classNames: ['inactive-date'],
+          };
+        } else {
+          return {
+            id: dateStr,
+            title: individualStock[dateStr].toString(),
+            start: dateStr,
+            backgroundColor: "#00E02E",
+            borderColor: "#00E02E",
+            extendedProps: {
+              description: "Disponible",
+            },
+          };
+        }
+      });
 
-      const reservedEvents = reservedDates.map((date) => ({
-        id: date,
-        title: "Indisponible",
-        start: date,
-        backgroundColor: "#FF0000",
-        borderColor: "#FF0000",
-        className: "",
-        editable: false,
-        extendedProps: {
-          description: "Indisponible",
-        },
-      }));
-
-      calendarApi.addEventSource([...events, ...reservedEvents]);
+      calendarApi.addEventSource(events);
       calendarApi.render();
     }
   };
 
-  const updateEventStock = (dateString: string, newStock: number) => {
-    const calendarApi = calendarApiRef.current;
-    const event = calendarApi.getEventById(dateString);
-    if (event) {
-      event.setProp("title", newStock.toString());
-      event.setExtendedProp(
-        "description",
-        newStock > 0 ? "Disponible" : "Indisponible"
-      );
-      event.setProp("backgroundColor", newStock > 0 ? "#00E02E" : "#FF0000");
-      event.setProp("borderColor", newStock > 0 ? "#00E02E" : "#FF0000");
-    }
-  };
-
-  const handlePrevNextClick = (arg: any) => {
-    const { view } = arg;
-    const newDate = view.activeStart;
-    const formattedNewDate = newDate.toISOString().split("T")[0];
-    console.log("Changing month to: ", formattedNewDate);
-    if (arg.type === "prev") {
-      calendarApiRef.current.prev();
-    } else if (arg.type === "next") {
-      calendarApiRef.current.next();
-    }
-    updateCalendarDates();
-  };
-
   const handleDateClick = (info: any) => {
     const clickedDate = new Date(info.dateStr);
-    const today = new Date();
-    today.setHours(0, 0, 0, 0);
-    clickedDate.setHours(0, 0, 0, 0);
+    const today = startOfToday();
 
-    if (clickedDate < today) {
-      error((props) => {}, {
-        title: "Erreur",
-        description: "Vous ne pouvez pas sélectionner une date passée.",
-      });
+    if (isPast(clickedDate) || isSunday(clickedDate)) {
+      // Ne rien faire pour les dates passées ou les dimanches
       return;
     }
 
-    const dayOfWeek = clickedDate.getDay();
-    const isSaturday = dayOfWeek === 6;
     const dateString = info.dateStr;
 
-    if ((dayOfWeek >= 1 && dayOfWeek <= 5) || isSaturday) {
-      const currentStock = individualStock[dateString] || stock;
-
-      if (selectedDates.includes(dateString)) {
-        removeDate(dateString, isSaturday);
-        const updatedSelectedDates = selectedDates.filter(
-          (date) => date !== dateString
-        );
-        setSelectedDates(updatedSelectedDates);
-        setStock((prevStock) => {
-          const newStock = Math.min(prevStock + 1, initialStock); // Ensure stock does not exceed initial stock
-          const newIndividualStock: Record<string, number> = {
-            ...individualStock,
-          };
-          delete newIndividualStock[dateString];
-
-          // Recalculer le stock des autres dates sélectionnées
-          let decrementedStock = initialStock;
-          updatedSelectedDates.forEach((date) => {
-            const recalculatedStock = decrementedStock--;
-            newIndividualStock[date] = recalculatedStock;
-            updateEventStock(date, recalculatedStock);
-          });
-
-          setIndividualStock(newIndividualStock);
-          updateEventStock(dateString, newStock);
-
-          return newStock;
+    if (selectedDates.includes(dateString)) {
+      removeDate(dateString, clickedDate.getDay() === 6);
+      setSelectedDates(selectedDates.filter((date) => date !== dateString));
+      setIndividualStock((prevStock) => ({
+        ...prevStock,
+        [dateString]: prevStock[dateString] + 1,
+      }));
+    } else {
+      if (individualStock[dateString] <= 0) {
+        error((props) => {}, {
+          title: "Erreur",
+          description: "Le stock est épuisé pour cette date.",
         });
-      } else {
-        if (stock <= 0) {
-          error((props) => {}, {
-            title: "Erreur",
-            description: "Le stock est épuisé pour cette date.",
-          });
-          return;
-        }
-
-        // Vérifiez le stock pour cette date spécifique
-        if (currentStock <= 0) {
-          error((props) => {}, {
-            title: "Erreur",
-            description: "Le stock pour cette date est épuisé.",
-          });
-          return;
-        }
-
-        addDate(dateString, isSaturday);
-        setSelectedDates([...selectedDates, dateString]);
-        setStock((prevStock) => {
-          const newStock = Math.max(prevStock - 1, 0); // Ensure stock is not negative
-          const newIndividualStock: Record<string, number> = {
-            ...individualStock,
-            [dateString]: Math.max(currentStock - 1, 0), // Ensure stock is not negative
-          };
-
-          // Recalculer le stock des autres dates sélectionnées
-          let decrementedStock = initialStock - 1;
-          const allSelectedDates = [...selectedDates, dateString];
-          allSelectedDates.sort(); // Tri des dates
-          allSelectedDates.forEach((date) => {
-            const recalculatedStock = decrementedStock--;
-            newIndividualStock[date] = recalculatedStock;
-            updateEventStock(date, recalculatedStock);
-          });
-
-          setIndividualStock(newIndividualStock);
-          updateEventStock(dateString, newIndividualStock[dateString]);
-
-          return newStock;
-        });
+        return;
       }
+
+      addDate(dateString, clickedDate.getDay() === 6);
+      setSelectedDates([...selectedDates, dateString]);
+      setIndividualStock((prevStock) => ({
+        ...prevStock,
+        [dateString]: prevStock[dateString] - 1,
+      }));
     }
   };
 
@@ -244,35 +150,23 @@ const Home: React.FC = () => {
         initialView="dayGridMonth"
         height={"auto"}
         dateClick={handleDateClick}
-        datesSet={handlePrevNextClick}
-        eventContent={({ event }) => (
-          <div className="min-h-14 border-0">
-            <div className="text-right p-2">
-              <span
-                className={`rounded-full inline-block px-2 py-1 ${
-                  event.extendedProps.description === "Indisponible"
-                    ? "bg-red-500"
-                    : event.extendedProps.description === "Passée"
-                      ? "bg-gray-500"
-                      : "bg-green-500"
-                }`}
-              >
-                {event.title}
-              </span>
+        eventContent={({ event }) => {
+          if (event.display === 'background') {
+            return null; // Ne rien afficher pour les dates inactives
+          }
+          return (
+            <div className="min-h-14 border-0">
+              <div className="text-right p-2">
+                <span className="rounded-full inline-block px-2 py-1 bg-green-500">
+                  {event.title}
+                </span>
+              </div>
+              <div className="p-2 text-center bg-green-500">
+                {event.extendedProps.description}
+              </div>
             </div>
-            <div
-              className={`p-2 text-center ${
-                event.extendedProps.description === "Indisponible"
-                  ? "bg-red-500"
-                  : event.extendedProps.description === "Passée"
-                    ? "bg-gray-500"
-                    : "bg-green-500"
-              }`}
-            >
-              {event.extendedProps.description}
-            </div>
-          </div>
-        )}
+          );
+        }}
       />
     </div>
   );
