72f01e3143c93044c7461ac9e66ff25d725a4695
mrvin100 <vincentyoumssi@gmail.com>
2024-08-27 12:30:07 +0100

    add client fisrt page

--- src/components/dashboard/professional/Clients.tsx
+++ src/components/dashboard/professional/Clients.tsx
@@ -1,12 +1,103 @@
+import { Input } from '@/components/ui/input';
 import { TabsContent } from '@/components/ui/tabs';
+import { ListFilter, PlusCircle, Search } from 'lucide-react';
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from '@/components/ui/button';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
+import Client from './Client';
+import { Separator } from '@/components/ui/separator';
 
 export default function ClientsTab() {
   return (
     <TabsContent value="clients" className="space-y-4">
-      <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
-        <div className="flex items-center justify-between space-y-2">
-          <h2 className="text-2xl font-bold tracking-tight">Clients</h2>
+      <div className="h-full flex-1 flex-col space-y-8 p-8 md:flex">
+        <div className="flex items-center justify-between space-y-2 flex-col">
+
+          <h2 className="text-2xl font-bold tracking-tight my-4">Clients</h2>
+
+          <div className='flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4 justify-between'>
+          <form className="mr-auto flex-1 sm:flex-initial">
+            <div className="relative">
+              <Search className="absolute right-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+              <Input
+                type="search"
+                placeholder="Rechercher..."
+                className="pr-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+              />
+            </div>
+          </form>
+          <div className="ml-auto flex items-center gap-2">
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="outline" size="sm" className="h-8 gap-1">
+                      <ListFilter className="h-3.5 w-3.5" />
+                      <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                      Trier
+                      </span>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>Trier par</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuCheckboxItem checked>
+                      Flair
+                    </DropdownMenuCheckboxItem>
+                    <DropdownMenuCheckboxItem>Boutique</DropdownMenuCheckboxItem>
+                    <DropdownMenuCheckboxItem>
+                      Autres
+                    </DropdownMenuCheckboxItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+                <Button size="sm" className="h-8 gap-1">
+                  <PlusCircle className="h-3.5 w-3.5" />
+                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                  Ajouter un client 
+                  </span>
+                </Button>
+          </div>
         </div>
+        <div className='border-y p-4 w-full'>
+          <Client />
+        </div>
+        {/* pagination */}
+        <div className='my-6'>
+        <Pagination>
+  <PaginationContent>
+    <PaginationItem>
+      <PaginationPrevious href="#" />
+    </PaginationItem>
+    <PaginationItem>
+      <PaginationLink href="#">1</PaginationLink>
+    </PaginationItem>    
+    <PaginationItem>
+      <PaginationLink href="#" isActive>2</PaginationLink>
+    </PaginationItem>
+    <PaginationItem>
+      <PaginationEllipsis />
+    </PaginationItem>
+    <PaginationItem>
+      <PaginationNext href="#" />
+    </PaginationItem>
+  </PaginationContent>
+</Pagination>
+        </div>
+      </div>
       </div>
     </TabsContent>
   );
