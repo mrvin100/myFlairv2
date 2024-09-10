30e2c91b67ea4d6fc2f8f68e7e67cffe7fa00914
mrvin100 <vincentyoumssi@gmail.com>
2024-09-10 16:49:44 +0100

    new add on reservation part

---
+++ src/components/dashboard/professional/calendar/Calendar.tsx
@@ -0,0 +1,11 @@
+import { TabsContent } from "@/components/tabs";
+
+const CalendarTab = () => {
+  return (
+    <TabsContent title="Calendar" value="calendar">
+      Calendar tab
+    </TabsContent>
+  );
+};
+
+export default CalendarTab;
