d4473272e5635a2298c407725336315c42aa3c86
mrvin100 <vincentyoumssi@gmail.com>
2024-09-25 16:49:37 +0100

    ajout des message par defaut sur le home du backup

--- src/app/back-up/page.tsx
+++ src/app/back-up/page.tsx
@@ -1,43 +1,20 @@
-'use client';
+"use client";
 
-import type { Service, User } from '@prisma/client';
+import type { Service, User } from "@prisma/client";
 
-import Image from 'next/image';
-import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import { getProfessionalsByTown, getAllServices } from '@/data/back-up';
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { getProfessionalsByTown, getAllServices } from "@/data/back-up";
 
-import { HeaderSection } from '@/components/shop/layout';
-import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import { HeaderSection } from "@/components/shop/layout";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 import { StarFilled } from "@ant-design/icons";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select';
-import { useUserContext } from '@/contexts/user';
-import Subscriptions from '@/components/back-up/Subscriptions';
-import { cn } from '@/lib/utils';
-import { ScrollArea } from '@/components/ui/scroll-area';
-import { CheckIcon, ChevronsUpDown } from 'lucide-react';
 
+import { useUserContext } from "@/contexts/user";
+import Subscriptions from "@/components/back-up/Subscriptions";
 interface Publication {
   id: string;
   name: string;
@@ -48,7 +25,7 @@ interface Publication {
   starRating: number;
   category: string;
   isAtHome: boolean;
-  idUser: string; 
+  idUser: string;
 }
 
 const ProfessionalDiscoverCard = () => {
@@ -56,11 +33,11 @@ const ProfessionalDiscoverCard = () => {
   useEffect(() => {
     const fetchServices = async () => {
       try {
-        const response = await fetch('/api/serviceProfessional/getAlea', {
+        const response = await fetch("/api/serviceProfessional/getAlea", {
           headers: {
-            'Cache-Control': 'no-cache',
-            'Pragma': 'no-cache',
-            'Expires': '0',
+            "Cache-Control": "no-cache",
+            Pragma: "no-cache",
+            Expires: "0",
           },
         });
         const data = await response.json();
@@ -73,11 +50,11 @@ const ProfessionalDiscoverCard = () => {
           prix: parseFloat(item.price),
           category: item.category,
           isAtHome: item.domicile,
-          idUser: item.user.id, 
+          idUser: item.user.id,
         }));
         setPublication(mappedData);
       } catch (error) {
-        console.error('Error fetching services:', error);
+        console.error("Error fetching services:", error);
       }
     };
 
@@ -85,60 +62,92 @@ const ProfessionalDiscoverCard = () => {
   }, []);
   function ModelPublication({ publication }: { publication: Publication }) {
     return (
-      <Card style={{ margin: 0 }} className='min-w-[330px] rounded-md'>
-        <div className='relative'>
+      <Card style={{ margin: 0 }} className="min-w-[330px] rounded-md">
+        <div className="relative">
           <Image
-            src={'/nail-salon.webp'}
+            src={"/nail-salon.webp"}
             width={1000}
             height={1000}
             alt="Picture of the author"
-            className='rounded-md object-cover'
+            className="rounded-md object-cover"
           />
-          <button style={{ padding: '9px', background: '#F8F8F8' }} className='absolute text-sm top-2 left-2 rounded-md text-black'>{publication.category}</button>
-          <img style={{ width: '40px', height: '40px', border: 'solid 2px white' }} className='object-cover absolute bottom-2 right-2 rounded-full' src={publication.imageProfil} alt="" />
+          <button
+            style={{ padding: "9px", background: "#F8F8F8" }}
+            className="absolute text-sm top-2 left-2 rounded-md text-black"
+          >
+            {publication.category}
+          </button>
+          <img
+            style={{ width: "40px", height: "40px", border: "solid 2px white" }}
+            className="object-cover absolute bottom-2 right-2 rounded-full"
+            src={publication.imageProfil}
+            alt=""
+          />
         </div>
         <br />
         <CardContent>
           <div>{publication.name}</div>
-          <div className='flex justify-between items-center'>
-            <div className='flex items-center' style={{ marginTop: '3%' }}>
-              <img src={'/iconService/map-pin-3.svg'} alt="map.icon" />
-              <span style={{ color: "#CECECE", marginLeft: '5px' }}>{publication.isAtHome ? (
-                <span style={{ color: "#CECECE" }}>À Domicile</span>
-              ) : (
-                <span style={{ color: "#CECECE" }}>{publication.ville}</span>
-              )},</span>
-              <span style={{ color: "#CECECE", marginLeft: '5px' }}>{publication.isAtHome ? (
-                <span style={{ color: "#CECECE" }}>{publication.ville}</span>
-              ) : (
-                <span style={{ color: "#CECECE" }}>{publication.pays}</span>
-              )}</span>
+          <div className="flex justify-between items-center">
+            <div className="flex items-center" style={{ marginTop: "3%" }}>
+              <img src={"/iconService/map-pin-3.svg"} alt="map.icon" />
+              <span style={{ color: "#CECECE", marginLeft: "5px" }}>
+                {publication.isAtHome ? (
+                  <span style={{ color: "#CECECE" }}>À Domicile</span>
+                ) : (
+                  <span style={{ color: "#CECECE" }}>{publication.ville}</span>
+                )}
+                ,
+              </span>
+              <span style={{ color: "#CECECE", marginLeft: "5px" }}>
+                {publication.isAtHome ? (
+                  <span style={{ color: "#CECECE" }}>{publication.ville}</span>
+                ) : (
+                  <span style={{ color: "#CECECE" }}>{publication.pays}</span>
+                )}
+              </span>
             </div>
-            <div className='flex items-center' style={{ color: '#CECECE', marginTop: '3%', marginRight: '2px' }}><StarFilled style={{ color: '#F7F74A', fontSize: '24px', marginRight: '5px' }} /> {publication.starRating}/5</div>
+            <div
+              className="flex items-center"
+              style={{ color: "#CECECE", marginTop: "3%", marginRight: "2px" }}
+            >
+              <StarFilled
+                style={{
+                  color: "#F7F74A",
+                  fontSize: "24px",
+                  marginRight: "5px",
+                }}
+              />{" "}
+              {publication.starRating}/5
+            </div>
           </div>
           <br />
-          <div className='flex justify-between'>
-            <span>A partir de <span style={{ fontWeight: '700', fontSize: '150%' }}>{publication.prix} €</span></span>
+          <div className="flex justify-between">
+            <span>
+              A partir de{" "}
+              <span style={{ fontWeight: "700", fontSize: "150%" }}>
+                {publication.prix} €
+              </span>
+            </span>
             <Button>Réserver</Button>
           </div>
         </CardContent>
       </Card>
-    )
+    );
   }
 
   return (
     <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {publication.map(pub => (
+      {publication.map((pub) => (
         <ModelPublication key={pub.id} publication={pub} />
       ))}
     </div>
   );
-}
+};
 
 export default function BackUpPage() {
   const { user } = useUserContext();
 
-  const [service, setService] = useState('');
+  const [service, setService] = useState("");
   const [options, setServices] = useState<Service[]>([]);
   const [professionals, setProfessionals] = useState<User[]>([]);
 
@@ -146,7 +155,7 @@ export default function BackUpPage() {
     (async () => {
       setServices(await getAllServices());
       setProfessionals(
-        await getProfessionalsByTown(user?.address?.town || 'Paris'),
+        await getProfessionalsByTown(user?.address?.town || "Paris")
       );
     })();
   }, [user]);
@@ -156,9 +165,7 @@ export default function BackUpPage() {
 
   return (
     <main>
-      <HeaderSection title="Trouvez le professionnel parfait">
-        
-      </HeaderSection>
+      <HeaderSection title="Trouvez le professionnel parfait"></HeaderSection>
 
       <div className="py-8">
         <section className="px-6 py-8 text-center lg:px-24">
@@ -168,28 +175,34 @@ export default function BackUpPage() {
           <p className="pb-16 pt-1 text-xs text-muted-foreground sm:text-sm">
             Trouvez les services professionnels à proximité
           </p>
-          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-4">
-            {options.map((service) => (
-              <Link
-                href={`/back-up/explore?service=${service.id}`}
-                key={service.id}
-              >
-                <Card className="flex flex-col items-center">
-                  <CardHeader>
-                    <Image
-                      src={service.image || '/default-service-image.jpg'}
-                      alt={service.title}
-                      width={1000}
-                      height={1000}
-                      className="w-full"
-                    />
-                  </CardHeader>
-                  <CardContent>
-                    <CardTitle className="mt-2">{service.title}</CardTitle>
-                  </CardContent>
-                </Card>
-              </Link>
-            ))}
+          <div>
+            {options && options.length > 0 ? (
+              <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-4">
+                {options.map((service) => (
+                  <Link
+                    href={`/back-up/explore?service=${service.id}`}
+                    key={service.id}
+                  >
+                    <Card className="flex flex-col items-center">
+                      <CardHeader>
+                        <Image
+                          src={service.image || "/default-service-image.jpg"}
+                          alt={service.title}
+                          width={1000}
+                          height={1000}
+                          className="w-full"
+                        />
+                      </CardHeader>
+                      <CardContent>
+                        <CardTitle className="mt-2">{service.title}</CardTitle>
+                      </CardContent>
+                    </Card>
+                  </Link>
+                ))}
+              </div>
+            ) : (
+              <div className="text-center">Aucun services professionnels présent.</div>
+            )}
           </div>
         </section>
 
@@ -202,8 +215,8 @@ export default function BackUpPage() {
           </p>
           <ProfessionalDiscoverCard />
           <br />
-          <div className='flex justify-center'>
-            <Link href={'/back-up/explore'}>
+          <div className="flex justify-center">
+            <Link href={"/back-up/explore"}>
               <Button>Voir plus</Button>
             </Link>
           </div>
--- src/components/back-up/Subscriptions.tsx
+++ src/components/back-up/Subscriptions.tsx
@@ -1,13 +1,12 @@
-'use client';
+"use client";
 
-import type { Subscription } from '@prisma/client';
+import type { Subscription } from "@prisma/client";
 
-import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import { CheckIcon, Cross2Icon } from '@radix-ui/react-icons';
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { CheckIcon, Cross2Icon } from "@radix-ui/react-icons";
 
-
-import { Button } from '@/components/ui/button';
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -15,17 +14,16 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from '@/components/ui/card';
+} from "@/components/ui/card";
 
 const types = {
-  MONTHLY: 'mois',
-  YEARLY: 'an',
+  MONTHLY: "mois",
+  YEARLY: "an",
 };
 
 export default function Subscriptions() {
   const [subscriptions, setSubscriptions] = useState<Subscription[]>([]);
 
- 
   return (
     <section className="px-6 py-8 text-center lg:px-24">
       <h2 className="text-3xl font-bold tracking-tight">
@@ -35,53 +33,63 @@ export default function Subscriptions() {
         Découvrez l&apos;abonnement qui s&apos;aligne parfaitement sur votre
         profession
       </p>
-      <div className="grid grid-cols-1 gap-4 px-16 lg:grid-cols-2 lg:px-24">
-        {subscriptions.map((subscription) => (
-          <Card key={subscription.id}>
-            <CardHeader>
-              <CardTitle className="text-xl">{subscription.title}</CardTitle>
-              <h2 className="text-3xl font-bold tracking-tight">
-                <b>
-                  {subscription.price}/{types[subscription.type]}
-                </b>
-              </h2>
-              <CardDescription>{subscription.description}</CardDescription>
-            </CardHeader>
+      <div>
+        {subscriptions && subscriptions.length > 0 ? (
+          <div className="grid grid-cols-1 gap-4 px-16 lg:grid-cols-2 lg:px-24">
+            {subscriptions.map((subscription) => (
+              <Card key={subscription.id}>
+                <CardHeader>
+                  <CardTitle className="text-xl">
+                    {subscription.title}
+                  </CardTitle>
+                  <h2 className="text-3xl font-bold tracking-tight">
+                    <b>
+                      {subscription.price}/{types[subscription.type]}
+                    </b>
+                  </h2>
+                  <CardDescription>{subscription.description}</CardDescription>
+                </CardHeader>
 
-            <CardContent>
-              <ul>
-                {subscription.arguments &&
-                  subscription.arguments.map((argument: any) => (
-                    <li
-                      className="flex items-center justify-center"
-                      key={argument.title}
-                    >
-                      {argument.type === 'POSITIVE' ? (
-                        <CheckIcon className="mr-2 h-4 w-4 text-green-500" />
-                      ) : (
-                        <Cross2Icon className="mr-2 h-4 w-4 text-red-500" />
-                      )}
-                      {argument.title}
-                    </li>
-                  ))}
-              </ul>
-            </CardContent>
+                <CardContent>
+                  <ul>
+                    {subscription.arguments &&
+                      subscription.arguments.map((argument: any) => (
+                        <li
+                          className="flex items-center justify-center"
+                          key={argument.title}
+                        >
+                          {argument.type === "POSITIVE" ? (
+                            <CheckIcon className="mr-2 h-4 w-4 text-green-500" />
+                          ) : (
+                            <Cross2Icon className="mr-2 h-4 w-4 text-red-500" />
+                          )}
+                          {argument.title}
+                        </li>
+                      ))}
+                  </ul>
+                </CardContent>
 
-            <CardFooter className="flex flex-col space-y-2">
-              <Link
-                className="w-full"
-                href={`/back-up/subscriptions/payment?type=${subscription.type.toLowerCase()}`}
-              >
-                <Button className="w-full" role="link" size="lg">
-                  Acheter
-                </Button>
-              </Link>
-              <div className="text-sm text-muted-foreground">
-                Abonnement sans engagement et résiliable sans frais
-              </div>
-            </CardFooter>
-          </Card>
-        ))}
+                <CardFooter className="flex flex-col space-y-2">
+                  <Link
+                    className="w-full"
+                    href={`/back-up/subscriptions/payment?type=${subscription.type.toLowerCase()}`}
+                  >
+                    <Button className="w-full" role="link" size="lg">
+                      Acheter
+                    </Button>
+                  </Link>
+                  <div className="text-sm text-muted-foreground">
+                    Abonnement sans engagement et résiliable sans frais
+                  </div>
+                </CardFooter>
+              </Card>
+            ))}
+          </div>
+        ) : (
+          <div className="text-center">
+            Aucun abonnements PRO présent.
+          </div>
+        )}
       </div>
     </section>
   );
