f5af4ba8f385d46779c2c2625013c65b2b2bf730
mrvin100 <vincentyoumssi@gmail.com>
2024-10-01 12:18:11 +0100

    ajout de modifs sur la page de serices additionels

--- src/app/shop/steps/additional-services/page.tsx
+++ src/app/shop/steps/additional-services/page.tsx
@@ -5,79 +5,76 @@ import CartGlobal from "../../Cart";
 import ButtonServiceAdd from "@/components/shop/steps/reservation/ButtonServiceAdd/page";
 import Link from "next/link";
 import CheckoutButton from "./CheckoutButton";
+import { Separator } from "@/components/ui/separator";
 
 export default async function AdditionalServices() {
   const additionalServices = (await getAllAdditionalServices()) || [];
 
   return (
-    <main className=" h-screen w-screen p-4">
-      <div className="flex justify-center items-center">
-        <img style={{ width: "70%" }} src="/logos/Rectangle_21.svg" alt="" />
-      </div>
-      <h1 className="font-bold text-2xl  text-center">
-        Il ne vous manques rien ?
-      </h1>
-      <div className=" mt-10 flex flex-col justify-center items-center ">
-        {additionalServices.map((additionalService) => (
-          <div key={additionalService.id} className="max-w-[700px]">
-            <div className="flex">
-              <Image
-                className="w-1/2 rounded-l-md"
-                src={additionalService.image}
-                alt={"imageOfService"}
-                width={1000}
-                height={1000}
-              />
+    <main className="min-h-screen w-full">
+      <div className="p-4 max-w-5xl mx-auto space-y-6">
+        <div className="flex justify-center items-center">
+          <img style={{ width: "70%" }} src="/logos/Rectangle_21.svg" alt="" />
+        </div>
+        <h1 className="text-2xl  text-center">
+          Il ne vous manques rien ?
+        </h1>
+        <div className="mt-10 flex flex-col justify-center items-center ">
+          {additionalServices.map((additionalService) => (
+            <div key={additionalService.id}>
+              <div className="flex flex-col md:flex-row max-w-md md:max-w-2xl">
+                <Image
+                  className="rounded-md md:rounded-l-md md:w-1/2 max-h-64 md:max-h-full"
+                  src={additionalService.image}
+                  alt={"imageOfService"}
+                  width={1000}
+                  height={1000}
+                />
 
-              <div className="w-1/2 bg-gray-100 p-4 rounded-r-md">
-                <h4 className="text-[24px] font-bold">
-                  {additionalService.title}
-                </h4>
-                <h3 className="flex text-gray-400 text-[20px]">
-                  {Intl.NumberFormat("fr-FR", {
-                    style: "currency",
-                    currency: "EUR",
-                  }).format(additionalService.price)}{" "}
-                  /{" "}
-                  {(() => {
-                    const typeLabels = {
-                      day: "jour",
-                      piece: "heure",
-                      page: "page"
-                    };
-                    return (
-                      <span className="text-gray-400 text-[20px] ml-1">
-                        {typeLabels[additionalService.type] || ""}
-                      </span>
-                    );
-                  })()}
-                </h3>
-                <p className="text-left">
-                  {additionalService.description}
-                </p>
-                <div className="flex justify-end">
-                  <ButtonServiceAdd service={additionalService} />
+                <div className=" md:w-1/2 bg-gray-100 p-4 rounded-r-md">
+                  <h4 className="text-[24px] font-bold">
+                    {additionalService.title}
+                  </h4>
+                  <h3 className="flex text-gray-400 text-[20px]">
+                    {Intl.NumberFormat("fr-FR", {
+                      style: "currency",
+                      currency: "EUR",
+                    }).format(additionalService.price)}{" "}
+                    /{" "}
+                    {(() => {
+                      const typeLabels = {
+                        day: "jour",
+                        piece: "heure",
+                        page: "page",
+                      };
+                      return (
+                        <span className="text-gray-400 text-[20px] ml-1">
+                          {typeLabels[additionalService.type] || ""}
+                        </span>
+                      );
+                    })()}
+                  </h3>
+                  <p className="text-left">{additionalService.description}</p>
+                  <div className="flex justify-end">
+                    <ButtonServiceAdd service={additionalService} />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
-
-      <div>
-        <div className="mt-10">
-          <h1 className="text-2xl font-bold">Panier</h1>
-          <br />
-          <CartGlobal />
+          ))}
         </div>
-      </div>
-      <div className="flex justify-end">
-        <Link href={"/shop/steps/business-boosters"}>
-          <Button className="mr-4" variant="secondary">
-            Annuler
-          </Button>
-        </Link>
-        <CheckoutButton />
+        <Separator className="my-12" />
+        <div>
+          <CartGlobal className="w-full mx-auto my-6" />
+        </div>
+        <div className="flex justify-end">
+          <Link href={"/shop/steps/business-boosters"}>
+            <Button className="mr-4" variant="secondary">
+              Annuler
+            </Button>
+          </Link>
+          <CheckoutButton />
+        </div>
       </div>
     </main>
   );
